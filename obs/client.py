2c491a1c1c81ac55cfd31b07bb1716ee3db0d068
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-07-02 11:08:53 +0700

    add user function

--- obs/client.py
+++ obs/client.py
@@ -1,9 +1,20 @@
+from .cloudian.user import User
+
+from .cloudian.requestors import CloudianRequestor
+
 class CloudianClient(object):
     def __init__(self, url, port, user, password):
         self._url = url
         self._port = port
         self._user = user
         self._password = password
+
+        self._requestor = CloudianRequestor(self._url,
+                                            self._port,
+                                            self._user,
+                                            self._password)
+
+        self.user = User(self._requestor)
 
 
 class S3Client(object):
