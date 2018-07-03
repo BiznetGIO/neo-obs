88a97b721e91f8b5760e579d68dfb8d43ecde034
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-07-03 11:37:14 +0700

    Add user delete qos

--- obs/cloudian/qos.py
+++ obs/cloudian/qos.py
@@ -24,3 +24,12 @@ class Qos(object):
                                                 method=method)
 
         return qos_details
+
+    def delete(self, data=None, json=None, method='POST'):
+        qos_details = CloudianRequestor.request(self.requestor,
+                                                url=self.base_url,
+                                                data=data,
+                                                json=json,
+                                                method=method)
+
+        return qos_details
