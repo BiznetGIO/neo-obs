c0ef961f13db2045c388f673d97b9d0fdc4bcdf8
iank <iank@localhost.localdomain>
2018-09-02 01:33:35 +0700

    Fix: Refactoring Cloudian User

--- obs/libs/cloudian/user.py
+++ obs/libs/cloudian/user.py
@@ -1,114 +1,53 @@
-from .requestors import CloudianRequestor
 from obs.libs.cloudian import requestors
 
-class User(object):
-    base_url = 'user'
 
-    def __init__(self, requestor):
-        self.requestor = requestor
-        self.credentials = Credentials(requestor)
+base_url = 'user'
 
-    def list(self, data=None, json=None, method='GET'):
-        base_url = self.base_url + '/list'
-        list_user = CloudianRequestor.request(self.requestor,
-                                              url=base_url,
-                                              data=data,
-                                              json=json,
-                                              method=method)
+def list(data=None, json=None, method='GET'):
+    base_url_list = base_url+'/list'
+    list_user = requestors.request(
+                        url=base_url_list,
+                        data=data,
+                        json=json,
+                        method=method)
 
-        return list_user
+    return list_user
 
-    def get(self, data=None, json=None, method='GET'):
-        get_user = CloudianRequestor.request(self.requestor,
-                                             url=self.base_url,
-                                             data=data,
-                                             json=json,
-                                             method=method)
+def get(data=None, json=None, method='GET'):
+    get_user = requestors.request(
+                        url=base_url,
+                        data=data,
+                        json=json,
+                        method=method)
 
-        return get_user
+    return get_user
 
-    def create(self, data=None, json=None, method='PUT'):
-        create_user = CloudianRequestor.request(self.requestor,
-                                                url=self.base_url,
-                                                data=data,
-                                                json=json,
-                                                method=method)
+def create(data=None, json=None, method='PUT'):
+    create_user = requestors.request(
+                            url=base_url,
+                            data=data,
+                            json=json,
+                            method=method)
 
-        return create_user
+    return create_user
 
-    def update(self, data=None, json=None, method='POST'):
-        update_user = CloudianRequestor.request(self.requestor,
-                                                url=self.base_url,
-                                                data=data,
-                                                json=json,
-                                                method=method)
+def update(data=None, json=None, method='POST'):
+    update_user = requestors.request(
+                                url=base_url,
+                                data=data,
+                                json=json,
+                                method=method)
 
-        return update_user
+    return update_user
 
-    def delete(self, data=None, json=None, method='DELETE'):
-        delete_user = CloudianRequestor.request(self.requestor,
-                                                url=self.base_url,
-                                                data=data,
-                                                json=json,
-                                                method=method)
+def delete(data=None, json=None, method='DELETE'):
+    delete_user = requestors.request(
+                                url=base_url,
+                                data=data,
+                                json=json,
+                                method=method)
 
-        return delete_user
+    return delete_user
 
 
-class Credentials(object):
-    def __init__(self, requestor):
-        self.requestor = requestor
 
-    def get(self, data=None, json=None, method='GET'):
-        credentials = CloudianRequestor.request(self.requestor,
-                                                url=User.base_url + '/credentials',
-                                                data=data,
-                                                json=json,
-                                                method=method)
-
-        return credentials
-
-    def list(self, data=None, json=None, method='GET'):
-        user_credentials = CloudianRequestor.request(self.requestor,
-                                                     url=User.base_url + '/credentials/list',
-                                                     data=data,
-                                                     json=json,
-                                                     method=method)
-
-        return user_credentials
-
-    def create(self, data=None, json=None, method='PUT'):
-        user_credentials = CloudianRequestor.request(self.requestor,
-                                                     url=User.base_url + '/credentials',
-                                                     data=data,
-                                                     json=json,
-                                                     method=method)
-
-        return user_credentials
-
-    def update(self, data=None, json=None, method='POST'):
-        user_credentials = CloudianRequestor.request(self.requestor,
-                                                     url=User.base_url + '/credentials',
-                                                     data=data,
-                                                     json=json,
-                                                     method=method)
-
-        return user_credentials
-
-    def status(self, data=None, json=None, method='POST'):
-        user_credentials = CloudianRequestor.request(self.requestor,
-                                                     url=User.base_url + '/credentials/status',
-                                                     data=data,
-                                                     json=json,
-                                                     method=method)
-
-        return user_credentials
-
-    def delete(self, data=None, json=None, method='DELETE'):
-        user_credentials = CloudianRequestor.request(self.requestor,
-                                                     url=User.base_url + '/credentials',
-                                                     data=data,
-                                                     json=json,
-                                                     method=method)
-
-        return user_credentials
