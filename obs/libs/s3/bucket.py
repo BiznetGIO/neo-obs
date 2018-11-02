662a08dc02abddcc17b7207c8c434cbd89ce46fd
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-11-02 18:39:10 +0700

    Edit: edit put_bucket into post_bucket as it will used to create bucket

--- obs/libs/s3/bucket.py
+++ obs/libs/s3/bucket.py
@@ -1,5 +1,10 @@
+from obs.libs.s3 import requestors
+import requests, os, json
+import botocore
+import boto3
 from obs.libs.s3 import login as login_lib
 from obs.libs.utils import log_utils
+
 
 def bucket_list(session=None):
     if not session:
@@ -8,19 +13,27 @@ def bucket_list(session=None):
     bucket = session.list_buckets()
     try:
         list_bucket = bucket.__getitem__('Buckets')
-        return list_bucket
+        return bucket
     except Exception as e:
         log_utils.log_err(e)
         exit()
 
 
-def put_bucket(session=None, name=None):
+def post_bucket(session=None, name=None, json=None):
     if not session:
         session = login_lib.get_client_session()
 
     try:
-        bucket = session.create_bucket(Bucket=name)
-        return bucket.__getitem__('Location')
+        if json is None:
+            bucket = session.create_bucket(Bucket=name)
+        else:
+            bucket = session.create_bucket(
+                Bucket=name,
+                **json
+            )
+
+        return bucket
+        
     except Exception as e:
         log_utils.log_err(e)
         exit()
