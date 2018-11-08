004da696396d05d42eecb6d1a86aefb4c1c4d907
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-11-08 13:53:39 +0700

    ADD: get object by type = default | acl | tagging | torrent

--- obs/libs/s3/object.py
+++ obs/libs/s3/object.py
@@ -13,3 +13,30 @@ def list_object(session=None, bucket=None):
     except Exception as e:
         log_utils.log_err(e)
         exit()
+
+
+def get_object(session=None, bucket=None, key=None, type=None):
+    if not session:
+        session = login_lib.get_client_session()
+
+    if type is None:
+        detail_object = session.get_object(Bucket=bucket, Key=key)
+    elif type == 'acl':
+        detail_object = session.get_object_acl(Bucket=bucket, Key=key)
+    elif type == 'tagging':
+        detail_object = session.get_object_tagging(Bucket=bucket, Key=key)
+    elif type == 'torrent':
+        detail_object = session.get_object_torrent(Bucket=bucket, Key=key)
+
+    return detail_object
+
+
+def put_object(session=None, bucket=None, key=None):
+    if not session:
+        session = login_lib.get_client_session()
+
+    object = session.put_object(Bucket=bucket, Key=key)
+
+    return object
+
+
