2028fac6a747258961e07cdc527a3eed465bd678
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-11-08 13:58:46 +0700

    EDIT: get object by type = default | acl | tagging | torrent

--- obs/libs/s3/object.py
+++ obs/libs/s3/object.py
@@ -15,7 +15,14 @@ def list_object(session=None, bucket=None):
         exit()
 
 
-def get_object(session=None, bucket=None, key=None, type=None):
+def get_object(session=None, bucket=None, key=None, gettype=None):
+    """
+    Get object function include several variable
+    session = session from where it called from
+    bucket = your bucket name
+    key = your object file name
+    gettype = your type of object result None|acl|tagging|torrent
+    """
     if not session:
         session = login_lib.get_client_session()
 
@@ -29,14 +36,3 @@ def get_object(session=None, bucket=None, key=None, type=None):
         detail_object = session.get_object_torrent(Bucket=bucket, Key=key)
 
     return detail_object
-
-
-def put_object(session=None, bucket=None, key=None):
-    if not session:
-        session = login_lib.get_client_session()
-
-    object = session.put_object(Bucket=bucket, Key=key)
-
-    return object
-
-
