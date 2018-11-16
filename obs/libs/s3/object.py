190f92491d5d8b53d80b396aa1f32aaaedd05766
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-11-16 14:58:07 +0700

    ADD: add put object multipart

--- obs/libs/s3/object.py
+++ obs/libs/s3/object.py
@@ -2,6 +2,7 @@ from obs.libs.s3 import login as login_lib
 from obs.libs.utils import log_utils
 import mimetypes
 import os
+from boto3.s3.transfer import TransferConfig
 
 
 def list_object(session=None, bucket=None):
@@ -73,6 +74,28 @@ def put_object(session=None, bucket=None, key=None, acl=None, tagging=None):
                                             Tagging=tagging)
 
         return object
+    except Exception as e:
+        log_utils.log_err(e)
+        exit()
+
+
+def put_object_multipart(session=None, bucket=None, key=None):
+    config = TransferConfig(multipart_threshold=1024 * 25,
+                            max_concurrency=10,
+                            multipart_chunksize=1024 * 25,
+                            use_threads=True)
+
+    if not session:
+        session = login_lib.get_client_session()
+
+    try:
+        with open(key, 'rb') as data:
+            object = session.upload_file(Filename=data.name, 
+                                        Bucket=bucket, 
+                                        Key=os.path.basename(data.name),
+                                        Config=config)
+
+            return object
     except Exception as e:
         log_utils.log_err(e)
         exit()
