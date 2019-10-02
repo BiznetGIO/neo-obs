0d2743d8a0dae5740213313d978e0f432a504ad5
azzamsa <azzam@azzamsa.com>
2019-10-02 12:28:17 +0700

    Fix: add case if policy file not set for gmt command

--- obs/storage/gmt.py
+++ obs/storage/gmt.py
@@ -5,6 +5,15 @@ from obs.libs import gmt
 
 def show_policies():
     policies = gmt.get_policies()
+    if policies == "notset":
+        docs_url = "#using-cloudian-hyperstore-extension"
+        msg = (
+            f"Can't find Policy file\n"
+            f"See '{docs_url}' in our documentation for more information"
+        )
+        click.secho(f"{msg}", fg="yellow", bold=True, err=True)
+        return
+
     try:
         for zone in policies:
             policy_id, description, _ = policies[zone].values()
