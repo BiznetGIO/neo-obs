655aa4193b8c0cd49c80f4a6d5251e454a612991
HuseinA <huseinabdulbar@gmail.com>
2019-11-04 17:31:00 +0700

    add test in directory libs

---
+++ tests/test_libs/test_config.py
@@ -0,0 +1,13 @@
+from obs.libs import config
+import os
+
+
+def test_config_file(monkeypatch):
+    monkeypatch.setattr(os.path, "expanduser", lambda comment: "home/hors")
+    assert config.config_file() == "home/hors/.config/neo-obs/obs.env"
+
+
+def test_exists(monkeypatch):
+    monkeypatch.setattr(config, "config_file", lambda: None)
+    monkeypatch.setattr(os.path, "isfile", lambda file: True)
+    assert config.is_config_exists() == True
---
+++ tests/test_libs/test_qos.py
@@ -0,0 +1,40 @@
+from obs.libs import qos
+from obs.libs import auth
+import mock, pytest
+
+
+@pytest.fixture
+def limit():
+    return {
+        "groupId": "testing",
+        "userId": "StageTest",
+        "labelId": "qos.userQosOverrides.title",
+        "qosLimitList": [
+            {"type": "STORAGE_QUOTA_KBYTES", "value": -1},
+            {"type": "REQUEST_RATE_LW", "value": -1},
+            {"type": "REQUEST_RATE_LH", "value": -1},
+            {"type": "DATAKBYTES_IN_LW", "value": -1},
+            {"type": "DATAKBYTES_IN_LH", "value": -1},
+            {"type": "DATAKBYTES_OUT_LW", "value": -1},
+            {"type": "DATAKBYTES_OUT_LH", "value": -1},
+            {"type": "STORAGE_QUOTA_COUNT", "value": -1},
+        ],
+    }
+
+
+def fake_client():
+    client = mock.Mock()
+    client.qos.limits.return_value = limit()
+    return client
+
+
+def test_info():
+    assert qos.info(fake_client(), "StageTest", "testing") == limit()
+
+
+def test_set():
+    assert qos.set(fake_client(), "user", "group", "limit") == limit()
+
+
+def test_rm():
+    assert qos.rm(fake_client(), "user", "group") == limit()
