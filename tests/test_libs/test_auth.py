9322caab978eb17ba8186b9b7c3566bb70937384
azzamsa <17734314+azzamsa@users.noreply.github.com>
2019-11-25 20:58:29 +0700

    Add: a feature choice of using https or http  (#22)
    * Fix: config prompts description
    
    * Add: a feature choice of using https or http
    
    * Fix: test that rely on get_endpoint

--- tests/test_libs/test_auth.py
+++ tests/test_libs/test_auth.py
@@ -5,6 +5,7 @@ from obs.libs import auth
 from obs.libs import config
 from cloudianapi.client import CloudianAPIClient
 from requests_aws4auth import AWS4Auth
+from obs.libs import auth as auth_lib
 
 
 def fake_config():
@@ -20,4 +21,5 @@ def fake_session(**kwargs):
 def test_resource(monkeypatch):
     monkeypatch.setattr(config, "load_config_file", fake_config)
     monkeypatch.setattr(boto3, "Session", fake_session)
+    monkeypatch.setattr(auth_lib, "get_endpoint", lambda: None)
     assert auth.resource() == "s3_resource"
